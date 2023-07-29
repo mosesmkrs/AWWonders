9c7d63872fd198febd57bfc2c18eba77e900273a
mosesmkrs <karisamoses392@gmail.com>
2023-07-29 18:37:43 +0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+const express = require('express')
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/userRoutes'); 
+const mongoose = require('mongoose')
+const cors = require('cors');
+
+
+
+
+//express app
+const app = express()
+
+//middleware
+app.use(express.json())
+app.use((req,res,next) => {
+    console.log(req.path,req.method)
+    next()
+})
+
+//cors
+app.use(cors({ origin: 'https://aw-wonders-frontend.vercel.app' }));
+
+   //routes
+app.use('/api/workouts/',workoutRoutes)
+app.use('/api/users/', userRoutes);
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    //listen for requests
+app.listen(process.env.PORT, () => {
+    console.log('connected to db & listening on port', process.env.PORT)
+})
+})
+.catch((error) => {
+    console.log(error)
+})
+
+module.exports = app;
+
+
