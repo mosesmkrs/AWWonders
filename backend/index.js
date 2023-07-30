11e70e7d9efa1892fea065aed5fc18fbe4aa3783
mosesmkrs <karisamoses392@gmail.com>
2023-07-30 05:35:26 +0300

    ghy

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use((req,res,next) => {
 })
 
 // the cors
-app.use(cors({ origin: 'https://aw-wonders.vercel.app/' }));
+app.use(cors());
 
    //routes
 app.use('/api/workouts',workoutRoutes)
