c9a47e82a17d2cf7453f4178d09c1c72b8f6a503
mosesmkrs <karisamoses392@gmail.com>
2023-07-30 05:22:23 +0300

    vft

--- backend/index.js
+++ backend/index.js
@@ -24,8 +24,8 @@ app.use((req,res,next) => {
 app.use(cors({ origin: 'https://aw-wonders.vercel.app/' }));
 
    //routes
-app.use('/api/workouts/',workoutRoutes)
-app.use('/api/users/', userRoutes);
+app.use('/api/workouts',workoutRoutes)
+app.use('/api/users', userRoutes);
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
